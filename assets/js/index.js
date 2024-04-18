572f9795eeba24a647a986a34364ad109dc0ddfc
spl-javmaldonado <javiera.maldonado.agurto@gmail.com>
2024-04-17 20:55:15 -0400

    septimo

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,6 +2,6 @@ const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]
 const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
 
 $("#enviar").click(function(){
-    alert("El correo fue enviado correctamente.");
+    alert("El mensaje fue enviado correctamente.");
   });
 
